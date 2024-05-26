c3840cb1b7d9855c9a90737c8295df4de8276814
Agustin Braco <bracoagustin@gmail.com>
2024-05-26 14:42:57 -0300

    [Main] - Create database and tables

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=e1bbbd&height=120&section=header"/>
+
+# Trabajo práctico integrador
+## Materia: Administración de base de datos
+
+<div>
+<h4>Grupo: Fuentón de achuras</h4>
+<h4>Integrantes: Agustín Braco, Ayelén Corrillo, Claudio Rodríguez, Daniel Drago, Justo Fleitas, Mercedes Marighetti, Tobias Arraiza.</h4>
+</div>
+
+<hr>
+
+<div>
+<h4>Descripción:</h4>
+<span>Base de datos para gestionar un negocio de alquiler de canchas hecho en SQL.</span>
+</div>
+
+<hr>
+
+<div>
+<p><b>¡Gracias!</b></p>  
+</div>
+
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=e1bbbd&height=120&section=footer"/>
--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,45 @@
+# Crear y usar base de datos
+CREATE DATABASE achuras;
+USE achuras;
 
+# Crear tablas
+CREATE TABLE IF NOT EXISTS usuarios(
+	id_usuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(255),
+	apellido VARCHAR(255),
+	dni int(8),
+	mail VARCHAR(255),
+	telefono INT(10)
+);
+
+CREATE TABLE IF NOT EXISTS canchas(
+	id_cancha INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	capacidad INT(2),
+	suelo VARCHAR(255),
+	techo BOOLEAN,
+	precio INT(6),
+	activa BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS administradores(
+	id_administrador INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(255),
+	apellido VARCHAR(255),
+	dni int(8),
+	mail VARCHAR(255),
+	telefono INT(10)
+);
+
+CREATE TABLE IF NOT EXISTS reservas(
+	id_reserva INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_usuario INT NOT NULL,
+	id_cancha INT NOT NULL,
+	id_administrador INT NOT NULL,
+	FOREIGN KEY (id_usuario) REFERENCES achuras.usuarios(id_usuario),
+	FOREIGN KEY (id_cancha) REFERENCES achuras.canchas(id_cancha),
+	FOREIGN KEY (id_administrador) REFERENCES achuras.administradores(id_administrador),
+	fecha DATE,
+	hora TIME,
+	precio INT(6),
+	abonada BOOLEAN DEFAULT FALSE
+);
